ed4cb3dc72a9b2bd893ce85515cc622b0242c30d
vladvlas <vladvlas175@gmail.com>
2022-04-23 10:53:30 +0300

    solved all tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,21 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let el in salaries) {
+    if (typeof salaries[el] === 'number' && salaries[el] != false && isNaN(salaries[el]) == false && isFinite(salaries[el]) == true) {
+      sum += salaries[el];
+    }
+    else {
+      continue;
+    }
+  }
+  return sum;
 }
+
+// let salaries = {
+//   John: 1000,
+//   Ann: 1600,
+//   Pete: 1300,
+//   month: 'December',
+//   currency: 'USD',
+//   isPayed: false
+// }
